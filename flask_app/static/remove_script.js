3cc418e63af897230401e733d8ae94a609b56b92
Spencer Rauch <rauchs@gmail.com>
2024-09-17 09:15:35 -0700

    additional language in confirmation prompt

--- flask_app/static/remove_script.js
+++ flask_app/static/remove_script.js
@@ -1,6 +1,6 @@
 function removeHandler(e, item) {
     console.log(item)
-    let confirmed = confirm(`Are you sure you want to remove ${item}?`)
+    let confirmed = confirm(`Are you sure you want to remove ${item} from the order entirely?`)
     if (!confirmed){
         e.preventDefault()
     }
