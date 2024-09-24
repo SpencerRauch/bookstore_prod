61f09f25951e24ce925969c74f83d1665b601ec0
Spencer Rauch <rauchs@gmail.com>
2024-09-24 12:21:03 -0700

    models and erd update for purchase orders

---
+++ flask_app/models/purchase_line_item_model.py
@@ -0,0 +1,124 @@
+from flask_app.config.mysqlconfig import connect_to_mysql
+from flask_app import DATABASE
+from flask_app.models import stockitem_model
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+
+
+class PurchaseLineItem:
+    def __init__(self,data) -> None:
+        self.id = data['id']
+        self.ordered_quantity = data['ordered_quantity']
+        self.received_quantity = data['received_quantity']
+        self.stock_item_id = data['stock_item_id']
+        self.sales_order_id = data['sales_order_id']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    @classmethod
+    def create(cls,data):
+        query = """
+            INSERT INTO purchase_line_items (ordered_quantity, sales_order_id, stock_item_id)
+            VALUES (%(ordered_quantity)s, %(sales_order_id)s, %(stock_item_id)s);
+        """
+        return connect_to_mysql(DATABASE).query_db(query,data)
+    
+    @classmethod
+    def update_ordered(cls,data):
+        query = """ 
+            UPDATE purchase_line_items SET ordered_quantity = %(ordered_quantity)s WHERE id = %(id)s;
+        """
+        return connect_to_mysql(DATABASE).query_db(query,data)
+
+    @classmethod
+    def update_received(cls,data):
+        query = """ 
+            UPDATE purchase_line_items SET received_quantity = %(received_quantity)s WHERE id = %(id)s;
+        """
+        return connect_to_mysql(DATABASE).query_db(query,data)
+    
+    @classmethod
+    def receive_full(cls, lines):
+        for line in lines:
+            if line.ordered_quantity == line.received_quantity:
+                pass
+            else:
+                cls.update_received({"id":line.id,"received_quantity":line.ordered_quantity})
+        
+    
+    @classmethod
+    def get_on_hand_by_line(cls,data):
+        query = """
+            SELECT stock_level FROM purchase_line_items
+            JOIN stock_items ON
+            stock_items.id = stock_item_id
+            WHERE purchase_line_items.id = %(id)s;
+        """
+        results = connect_to_mysql(DATABASE).query_db(query,data)
+        if results:
+            return results[0]['stock_level']
+        return False
+    
+    @classmethod
+    def delete(cls,data):
+        query = """
+            DELETE FROM purchase_line_items WHERE id = %(id)s;
+        """
+        return connect_to_mysql(DATABASE).query_db(query,data)
+
+    @classmethod
+    def get_all_for_order(cls, data):
+        query = """
+            SELECT * FROM purchase_line_items
+            JOIN stock_items
+            ON stock_items.id = stock_item_id
+            WHERE sales_order_id = %(id)s;
+        """
+        results = connect_to_mysql(DATABASE).query_db(query,data)
+        all_items = []
+        for row in results:
+            one_item = cls(row)
+            one_item.stock_item = row['name']
+            one_item.on_hand = row['stock_level']
+            all_items.append(one_item)
+        return all_items
+        
+    
+    @classmethod
+    def get_by_id(cls,data):
+        query = """
+            SELECT * FROM purchase_line_items WHERE purchase_line_items.id = %(id)s;
+        """
+        results = connect_to_mysql(DATABASE).query_db(query,data)
+        if results:
+            return cls(results[0])
+        return False
+    
+    
+    @staticmethod
+    def valid_sale_line_item(data):
+        is_valid = True
+        if len(data['ordered_quantity']) < 1:
+            is_valid = False
+            flash('Quantity required','new_line')
+        if len(data['stock_item_id']) < 1:
+            is_valid = False
+            flash('Item required','new_line')
+        else:
+            potential_stock_item = stockitem_model.StockItem.get_by_id({'id':data['stock_item_id']})
+            if not potential_stock_item:
+                is_valid = False
+                flash('item not found','new_line')
+        try:
+            int_qty = int(data['ordered_quantity'])
+            if int_qty < 0:
+                flash("quantity must be positive",'new_line')
+                is_valid = False
+        except ValueError:
+            flash("quantity should be numerical and positive",'new_line')
+            is_valid = False
+        return is_valid
+
--- flask_app/models/purchase_order_model.py
+++ flask_app/models/purchase_order_model.py
@@ -1,14 +1,14 @@
 from flask_app.config.mysqlconfig import connect_to_mysql
 from flask_app import DATABASE
-from flask_app.models import sale_line_item_model
-from flask_app.models import customer_model
+# from flask_app.models import sale_line_item_model
+from flask_app.models import manufacturer_model
 from flask import flash
 import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
 
 
-class SalesOrder:
+class PurchaseOrder:
     statuses = {
         0: 'entering',
         1: 'ordered',
@@ -16,57 +16,58 @@ class SalesOrder:
         3: 'cancelled'
     }
     
-    def __init__(self,data,with_items=False, with_customer=False) -> None:
+    def __init__(self,data,with_items=False, with_manufacturer=False) -> None:
         self.id = data['id']
         self.status = data['status']
         self.employee_id = data['employee_id']
-        self.customer_id = data['customer_id']
+        self.manufacturer_id = data['manufacturer_id']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-        if with_customer:
-            self.customer = customer_model.Customer.get_by_id({'id':self.customer_id})
+        if with_manufacturer:
+            self.manufacturer = manufacturer_model.Manufacturer.get_by_id({'id':self.manufacturer_id})
         if with_items:
-            self.items = sale_line_item_model.SaleLineItem.get_all_for_order({'id':self.id})
+            # self.items = sale_line_item_model.SaleLineItem.get_all_for_order({'id':self.id})
+            pass
 
 
 
     @classmethod
     def create(cls,data):
         query = """
-            INSERT INTO sale_orders (employee_id, customer_id)
-            VALUES (%(employee_id)s, %(customer_id)s);
+            INSERT INTO purchase_orders (employee_id, manufacturer_id)
+            VALUES (%(employee_id)s, %(manufacturer_id)s);
         """
         return connect_to_mysql(DATABASE).query_db(query,data)
     
     @classmethod
     def update_status(cls,data):
         query = """ 
-            UPDATE sale_orders SET status = %(status)s WHERE id = %(id)s;
+            UPDATE purchase_orders SET status = %(status)s WHERE id = %(id)s;
         """
         return connect_to_mysql(DATABASE).query_db(query,data)
     
     @classmethod
-    def get_all_with_customer(cls):
+    def get_all_with_manufacturer(cls):
         query = """
-            SELECT sale_orders.*, customers.*, COUNT(sale_line_items.id) as item_count FROM sale_orders
-            JOIN customers
-            ON customer_id = customers.id
-            LEFT JOIN sale_line_items
-            ON sale_line_items.sales_order_id = sale_orders.id
-            GROUP BY sale_orders.id
-            ORDER BY sale_orders.id DESC;
+            SELECT purchase_orders.*, manufacturers.*, COUNT(purchase_sale_items.id) as item_count FROM purchase_orders
+            JOIN manufacturers
+            ON manufacturer_id = manufacturers.id
+            LEFT JOIN purchase_sale_items
+            ON purchase_sale_items.sales_order_id = purchase_orders.id
+            GROUP BY purchase_orders.id
+            ORDER BY purchase_orders.id DESC;
         """
         results = connect_to_mysql(DATABASE).query_db(query)
         all_items = []
         for row in results:
             one_order = cls(row)
-            cust_data = {
+            manu_data = {
                 **row,
-                'id': row['customers.id'],
-                'created_at':row['customers.created_at'],
-                'updated_at':row['customers.updated_at']
+                'id': row['manufacturers.id'],
+                'created_at':row['manufacturers.created_at'],
+                'updated_at':row['manufacturers.updated_at']
             }
-            one_order.customer = customer_model.Customer(cust_data)
+            one_order.manufacturer = manufacturer_model.Manufacturer(manu_data)
             one_order.item_count = row['item_count']
             all_items.append(one_order)
         return all_items
@@ -75,7 +76,7 @@ class SalesOrder:
     @classmethod
     def get_by_id(cls,data):
         query = """
-            SELECT * FROM sale_orders WHERE sale_orders.id = %(id)s;
+            SELECT * FROM purchase_orders WHERE purchase_orders.id = %(id)s;
         """
         results = connect_to_mysql(DATABASE).query_db(query,data)
         if results:
@@ -86,7 +87,7 @@ class SalesOrder:
     @staticmethod
     def valid_sale_order(data):
         is_valid = True
-        if len(data['customer_id']) < 1:
+        if len(data['manufacturer_id']) < 1:
             is_valid = False
             flash('Customer required')
         # potential_sale_order = Manufacturer.get_by_name({'name':data['name']})
